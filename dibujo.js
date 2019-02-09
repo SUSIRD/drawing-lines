128640562206a867e84ccb91913b9a9e79339496
susird <susir95@gmail.com>
2019-02-09 16:01:38 -0500

    more lines

--- dibujo.js
+++ dibujo.js
@@ -1,9 +1,30 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
+var xi, xx;
 
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("yellow", 310, 10, 10, 220);
-dibujarLinea("purple", 10, 200, 300, 350);
+for(l=0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    xy = 10 * l;
+    xx = 10 * (l + 1);
+    wy = 300 - xf;
+    wx = 300 - xx;
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    dibujarLinea(colorcito, 300, xy, xx, 0);
+    dibujarLinea(colorcito, xy, 300, 300, wx);
+    dibujarLinea(colorcito, 0, yi, wy, 0);
+   
+    console.log("linea" + l)
+    
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 300)
+dibujarLinea(colorcito, 1, 299, 299, 299)
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
  {
